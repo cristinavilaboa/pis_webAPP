05cdd5348f2eed998f4835e209dd41e162607b5d
Vero <Vero@Vero-PC.guest.fing.edu.uy>
2016-10-29 18:35:39 -0300

    Merge con lo de Paula

---
+++ WebProfesor/WebContent/js/header.js
@@ -0,0 +1,23 @@
+$(function () {
+	$("#mensajes a:contains('Mensajes')").parent().addClass("active");
+	$("#index a:contains('Inicio')").parent().addClass("active");
+	$("#estadisticas a:contains('Estadisticas')").parent().addClass("active");
+	$("#addMundo a:contains('Mundo')").parent().addClass("active");
+	$("#addNivel a:contains('Nivel')").parent().addClass("active");
+	$("#addProblema a:contains('Problema')").parent().addClass("active");
+	
+	if($("#addMundo a:contains('Mundo')").parent().hasClass("active")){
+		$(".dropdown a:contains('Nuevo')").parent().addClass("active");
+	}
+	
+	if($("#addNivel a:contains('Nivel')").parent().hasClass("active")){
+		$(".dropdown a:contains('Nuevo')").parent().addClass("active");
+	}
+	
+	if($("#addProblema a:contains('Problema')").parent().hasClass("active")){
+		$(".dropdown a:contains('Nuevo')").parent().addClass("active");
+	}
+	
+	
+	
+});
